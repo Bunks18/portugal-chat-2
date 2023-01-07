5978bb1c3df85f7932e4309b22c5643a3a4f8afe
bunks18 <wbunker11@gmail.com>
2023-01-07 11:59:11 -0700

    feat: add audio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import nightCafe from "./night_cafe.png";
+import nightCafeAudio from "./coimbra_plaza_vid.m4v";
 import "./App.css";
 
 function App() {
@@ -6,6 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={nightCafe} className="App-logo" alt="logo" />
+        <video
+          src={nightCafeAudio}
+          width="600"
+          height="300"
+          controls="controls"
+          autoplay="true"
+        />
+
         <p>this is a change</p>
       </header>
     </div>
